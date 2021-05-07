128fa91e131839ea46a7d7335d946b53765fe9f5
Cole Broderick <cole.broderick@snhu.edu>
2021-05-07 11:54:52 -0500

    checking

--- src/cs_capstone/CS_Capstone.java
+++ src/cs_capstone/CS_Capstone.java
@@ -12,8 +12,6 @@ package cs_capstone;
 public class CS_Capstone {
     
     public static void main(String[] args) {
-        
-        //Just making sure git is correct 
         FlightData flight1 = new FlightData("flight_1", "10:00:00", "12:00:00", "aircraft_1", 100, 143);
         FlightData flight2 = new FlightData("flight_2", "07:00:00", "13:00:00", "aircraft_2", 121, 143);
         FlightData flight3 = new FlightData("flight_3", "16:00:00", "19:00:00", "aircraft_1", 166, 175);
