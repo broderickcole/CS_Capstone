26527d93b989d064d26182cf3b0f28ef398696f3
Cole Broderick <cole.broderick@snhu.edu>
2021-05-07 12:11:50 -0500

    IsolationTest

---
+++ src/cs_capstone/EditingInIsolation.java
@@ -0,0 +1,27 @@
+
+package cs_capstone;
+
+/**
+ *
+ * @author colebroderick
+ */
+public class EditingInIsolation extends FlightData {
+    
+    private int nubTest;
+
+    public EditingInIsolation(int nubTest, String flightId, String departureTime, String arrivalTime, String assignedAircraftId, double passengerCount, double totalSeats) {
+        super(flightId, departureTime, arrivalTime, assignedAircraftId, passengerCount, totalSeats);
+        this.nubTest = nubTest;
+    }
+
+    public int getNubTest() {
+        return nubTest;
+    }
+
+    public void setNubTest(int nubTest) {
+        this.nubTest = nubTest;
+    }
+
+
+    
+}
