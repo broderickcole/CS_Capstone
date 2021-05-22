59fa95d2b8234ec63c9a8ccb720b718f0fbc96e0
Cole Broderick <cole.broderick@snhu.edu>
2021-05-21 21:04:55 -0500

    Fixing client IP

--- src/cs_capstone/CS_Capstone.java
+++ src/cs_capstone/CS_Capstone.java
@@ -12,21 +12,20 @@ public class CS_Capstone {
 
     //Creating new objects in my flight data classes that can later be called on 
     public static void main(String[] args) {
-                
-                // Creating a Mongo client 
+
+        // Creating a Mongo client 
         MongoClient mongo = new MongoClient("18.116.200.95", 27017);
 
         // Creating Credentials 
         MongoCredential credential;
-        credential = MongoCredential.createCredential("FlightData", "myDb",
+        credential = MongoCredential.createCredential("Cole Broderick", "myDb",
                 "password".toCharArray());
         System.out.println("Connected to the database successfully");
 
         // Accessing the database 
         MongoDatabase database = mongo.getDatabase("myDb");
         System.out.println("Credentials ::" + credential);
-        
-        
+
         FlightData flight1 = new FlightData("flight_1", "10:00:00", "12:00:00", "aircraft_1", 100, 143);
         FlightData flight2 = new FlightData("flight_2", "07:00:00", "13:00:00", "aircraft_2", 121, 143);
         FlightData flight3 = new FlightData("flight_3", "16:00:00", "19:00:00", "aircraft_1", 166, 175);
