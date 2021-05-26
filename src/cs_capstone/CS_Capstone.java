67573adef6811eeca7e02b50f6a00e585c99b0a7
Cole Broderick <cole.broderick@snhu.edu>
2021-05-26 18:52:17 -0500

    Test

--- src/cs_capstone/CS_Capstone.java
+++ src/cs_capstone/CS_Capstone.java
@@ -2,8 +2,11 @@ package cs_capstone;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
+import java.util.Iterator;
+import org.bson.Document;
 
 /**
  *
@@ -15,26 +18,29 @@ public class CS_Capstone {
     public static void main(String[] args) {
 
         // Creating a Mongo client 
-        MongoClient mongo = new MongoClient("mongodb://Cole:Broderick@3.15.9.1/flights");
-
+        MongoClient mongoClient = new MongoClient("mongodb://Cole:Broderick@3.15.9.1");
+        
         // Accessing the database 
-        MongoDatabase database = mongo.getDatabase("FlightData");
+        MongoDatabase database = mongoClient.getDatabase("flights");
         System.out.println("Database Name: " + database.getName());
+        
+      //I guess im just confused on this part. 
+      
+      //MongoCollection<Document> collection = database.getCollection("myCollection"); 
+      // I havent programmed with java in a whiel and for some reason get confused when they use the words in replace of the code. I never k
+      MongoCollection<Document> collection = database.getCollection("myCollection");
+      //Retrieving the documents
+      FindIterable<Document> iterDoc = collection.find();
+      Iterator it = iterDoc.iterator();
+      while (it.hasNext()) {
+         System.out.println(it.next());
+      }
 
-        // Creating Credentials 
-        MongoCredential credential;
-        credential = MongoCredential.createCredential("Cole Broderick", "myDb",
-                "password".toCharArray());
-        System.out.println("Credentials ::" + credential);
-        System.out.println("Connected to the database successfully");
 
-        // Creating a collection
-        database.createCollection("sampleCollection");
-        System.out.println("Collection created successfully");
 
         FlightData flight1 = new FlightData("flight_1", "10:00:00", "12:00:00", "aircraft_1", 100, 143);
         FlightData flight2 = new FlightData("flight_2", "07:00:00", "13:00:00", "aircraft_2", 121, 143);
-        FlightData flight3 = new FlightData("flight_3", "16:00:00", "19:00:00", "aircraft_1", 166, 175);
+        FlightData flight3 = new FlightData("flight_3", "16:show 00:00", "19:00:00", "aircraft_1", 166, 175);
         FlightData flight4 = new FlightData("flight_1", "13:00:00", "17:00:00", "aircraft_4", 147, 175);
 
         // Creating an array of the objects
